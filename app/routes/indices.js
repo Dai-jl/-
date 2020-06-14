3d98553b8a0dcebcad9bef02aba28b37acc13f7f
Broca <z502723158@gmail.com>
2020-06-15 04:14:00 +0800

    change indices to ramda, format readme.md

--- app/routes/indices.js
+++ app/routes/indices.js
@@ -6,36 +6,42 @@ var query = require('../db.js');
 router.get('/test', function(req, res, next) {
   res.send('respond with a resource');
 });
+var R = require('ramda')
 
-function formatIndex(data){
-	console.log(data)
-	var res = [];
-	for (i in data){
-		var t={};
-		t.type = i;
-		t.index = data[i];
-		res.push(t);
-	}
-	console.log(res);
-	return res;
-}
+// function formatIndex(data){
+// 	var res = [];
+// 	for (i in data){
+// 		var t={};
+// 		t.type = i;
+// 		t.index = data[i];
+// 		res.push(t);
+// 	}
+// 	return res;
+// }
 
 var sql = 'select type_name,index_name from job_type t, job_index i where i.type_id = t.id'
 
-
+const byTypeName = R.groupBy(function (row){
+    return row.type_name;
+})
+var result = []
+function deal (value,key){
+    result.push({"type":key,"index":R.pluck('index_name')(value)})
+}
 router.get('/',async(req,res)=>{
-	let data = {};
+	// let data = {};
 	const rows = await query(sql);
-	for(row of rows){
-	  	if(!data[row.type_name]){
-	  		data[row.type_name] = [];
-	  	}
-	  	data[row.type_name].push(row.index_name);
-	}
-	console.log(data);
-	data = formatIndex(data);
-	console.log(data);
-	res.json(data);
+	//before:
+	// for(row of rows){
+	//   	if(!data[row.type_name]){
+	//   		data[row.type_name] = [];
+	//   	}
+	//   	data[row.type_name].push(row.index_name);
+	// }
+	// data = formatIndex(data);
+	//after:
+	R.forEachObjIndexed(deal,byTypeName(rows))
+	res.json(result);
 })
 
 module.exports = router;
